ef7e2f7a863dce90a69237729be82bff505f3974
John Smoker <testgits@yandex.ru>
2021-03-16 18:49:01 +0500

    Direct open selenide page

--- src/test/java/tests/GithubTests.java
+++ src/test/java/tests/GithubTests.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -18,18 +17,10 @@ public class GithubTests {
 
     @Test
     void findSelenideInGtihub() {
-        //Откройте страницу Selenide в Github
-        open("https://github.com/");
-        $(byName("q")).setValue("selenide").pressEnter();
-        $("[href='/selenide/selenide'").click();
+        open("https://github.com/selenide/selenide");
 
-        //Перейдите в раздел Wiki проекта
         $("[href='/selenide/selenide/wiki'").click();
-
-        //Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
         $("#wiki-body").shouldHave(text("Soft assertions"));
-
-        //Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
         $(byText("Soft assertions")).click();
         $("#wiki-body").shouldHave(text("Using JUnit5"), text("@ExtendWith"));
     }
